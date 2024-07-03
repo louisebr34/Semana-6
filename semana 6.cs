fadbb0bc67e5d2eaf661e647ea922d1f74320f8f
Louise <166768872+louisebr34@users.noreply.github.com>
2024-07-03 12:43:34 -0300

    Add files via upload

---
+++ semana 6.cs
@@ -0,0 +1,14 @@
+﻿namespace identifica.cs
+{
+    public class Class1
+    {
+
+    }
+}
+namespace Semana_06.Interface
+{
+    internal interface IIdentifica
+    {
+        string Identificar();
+    }
+}
